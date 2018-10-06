a1750cbf7ccbafc1eae8cc9e8fdb5a7dcfbc3532
Benibr <dev0@domainmess.org>
2018-10-06 22:56:25 +0200

    added write functionality:w

--- beni_first_ocaml.ml
+++ beni_first_ocaml.ml
@@ -45,19 +45,30 @@ let list_file file =
   List.rev !lines ;;
 
 let find_configfile interface = 
-  let configfile = "./test" in
-  Logs.info (fun m -> m "not implemented yet, static '%s'" configfile);
-  configfile
+  let default = "./test" in
+  let path = String.concat "" [ "/etc/wpa_supplicant/wpa_supplicant-"; interface; ".conf" ] in
+  if Sys.file_exists path then
+    Logs.debug (fun m -> m "found configfile for interface %s %s" interface path);
+  let path = String.concat "" [ "/etc/wpa_supplicant-"; interface; ".conf" ] in
+  if Sys.file_exists path then
+    Logs.debug (fun m -> m "found configfile for interface %s %s" interface path);
+  default
 
 let write_config network file =
-  Logs.info (fun m -> m "not implemented yet")
+  Logs.debug (fun m -> m "appending to file %s" file);
+  let write_chan = open_out_gen [Open_append] 0o664 file in
+  output_string write_chan "\n";
+  List.iter (output_string write_chan) network;
+  close_out write_chan
 
+let example_net = [ "network={\n"; "\tssid=\"karlsruhe.freifunk.net\"\n"; "\tkey_mgmt=NONE\n"; "\tauth_alg=OPEN\n"; "}\n"]
 
 (* main loop *)
 
 let main () =
   let interface = find_interface in
   let configfile = find_configfile interface in
+  write_config example_net configfile;
   let config = list_file configfile in
   List.iter print_endline config
 
