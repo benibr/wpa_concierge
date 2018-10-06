115165de2ac92a81fcbab6f05fa3a4f9d78ae260
Benibr <dev0@domainmess.org>
2018-10-06 20:45:37 +0200

    basic layout set

--- beni_first_ocaml.ml
+++ beni_first_ocaml.ml
@@ -1,5 +1,16 @@
 open Sys
 
+(* network device handling *)
+
+(* iwlist scan 
+ *
+ *)
+let find_interface = 
+  let interface = "wlan0" in
+  Logs.debug (fun m -> m "found the following wlan interface %s" interface);
+  interface
+
+
 (* iwlist scan 
  *
  *)
@@ -14,6 +25,9 @@ let iwlist_scan =
   with End_of_file ->
   close_in iwconfig_chan;
   List.rev !lines ;;
+
+
+(* Configfile handling *)
 
 (* list_file
  * opens a file, read it line by line and returns the content as a list
@@ -30,15 +44,21 @@ let list_file file =
   close_in read_chan;
   List.rev !lines ;;
 
-let find_config =
-  Logs.info (fun m -> m "not implemented yet")
+let find_configfile interface = 
+  let configfile = "./test" in
+  Logs.info (fun m -> m "not implemented yet, static '%s'" configfile);
+  configfile
 
 let write_config network file =
   Logs.info (fun m -> m "not implemented yet")
 
+
 (* main loop *)
+
 let main () =
-  let conf = list_file "/home/benibr/Workspace/marakkesh/beni_first_ocaml/test" in
-  List.iter print_endline conf
+  let interface = find_interface in
+  let configfile = find_configfile interface in
+  let config = list_file configfile in
+  List.iter print_endline config
 
 let () = main ()
