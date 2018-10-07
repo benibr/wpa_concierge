70abba99b98e9acc53498660cfb2ab33610f66b1
Benibr <dev0@domainmess.org>
2018-10-07 17:26:25 +0200

    added cli cmds

--- wpa_concierge.ml
+++ wpa_concierge.ml
@@ -44,6 +44,7 @@ let list_file file =
   close_in read_chan;
   List.rev !lines ;;
 
+(* genereate a configfilename form interfacename and test if file exists *)
 let find_configfile interface = 
   let default = "./test" in
   let path = String.concat "" [ "/etc/wpa_supplicant/wpa_supplicant-"; interface; ".conf" ] in
@@ -54,22 +55,41 @@ let find_configfile interface =
     Logs.debug (fun m -> m "found configfile for interface %s %s" interface path);
   default
 
+(* plain write list of options to file *)
 let write_config network file =
   Logs.debug (fun m -> m "appending to file %s" file);
   let write_chan = open_out_gen [Open_append] 0o664 file in
   output_string write_chan "\n";
   List.iter (output_string write_chan) network;
-  close_out write_chan
+  close_out write_chan;;
 
-let example_net = [ "network={\n"; "\tssid=\"karlsruhe.freifunk.net\"\n"; "\tkey_mgmt=NONE\n"; "\tauth_alg=OPEN\n"; "}\n"]
+let generate_config =
+(* example data *)
+[ "network={\n"; "\tssid=\"karlsruhe.freifunk.net\"\n"; "\tkey_mgmt=NONE\n"; "\tauth_alg=OPEN\n"; "}\n"]
+
+(* parse cmd options *)
+let select_workmode arg =
+  match arg with
+  | "disconnect" -> Printf.printf "disconnecting not implemented"
+  | "scan" -> Printf.printf "scanning not implemented"
+  | "scanning" -> Printf.printf "scanning not implemented"
+  | _ -> write_config generate_config (find_configfile find_interface)
+;;
 
 (* main loop *)
 
-let main () =
-  let interface = find_interface in
+(*let main () =
   let configfile = find_configfile interface in
   write_config example_net configfile;
   let config = list_file configfile in
   List.iter print_endline config
+*)
+
+let main () =
+  let interface = find_interface in
+  let workmode = 
+    try Sys.argv.(1)
+    with Invalid_argument("index out of bounds") -> "connect" in 
+  select_workmode workmode
 
 let () = main ()
